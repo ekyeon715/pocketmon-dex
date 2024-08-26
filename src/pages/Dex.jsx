edb791abcbad1780cb5bfdc808d9fc28e98ce41e
ekyeon715 <rkdwldus9636@naver.com>
2024-08-26 21:20:46 +0900

    fix: 불필요한 코드 제거

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,7 +1,6 @@
 import React, { createContext, useState } from "react";
 import Dashboard from "../components/Dashboard";
 import PokemonList from "../components/PokemonList";
-import MOCK_DATA from "../mock";
 
 export const PokemonContext = createContext();
 
